3499e42edc23020bb327759faea4ff3999f8eec7
Amadou Kane <ak45amad@hotmail.fr>
2018-11-20 22:10:28 +0100

    Ajout bootstrap & jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,16 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+const $ = require('jquery');
+
+require('bootstrap');
 
 // any CSS you require will output into a single css file (app.scss in this case)
 require('../css/app.scss');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
