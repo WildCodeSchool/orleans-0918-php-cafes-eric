a5448cd554283bf16e4c369ac284e72cdd1c8309
Melissa Viltet <melissaviltet@gmail.com>
2018-11-23 15:35:31 +0100

    changement éffectuer, pardon pour cet oublie

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,4 +18,4 @@ $(document).ready(function() {
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
