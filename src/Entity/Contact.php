a24807f7f131cf320816336c7102840ca3eb6b04
Melissa Viltet <melissaviltet@gmail.com>
2018-11-29 11:49:35 +0100

    Formulaire de contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,134 @@
+<?php
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact{
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank
+     * @Assert\Length(max="50")
+     */
+    private $firstname;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank
+     * @Assert\Length(max="50")
+     */
+    private $name;
+    /**
+     * @var string|null
+     * @Assert\NotBlank
+     * @Assert\Regex(
+     *     pattern="/[0-9]{10}/"
+     * )
+     */
+    private $phone;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank
+     * @Assert\Length(min="10")
+     */
+    private $message;
+
+    /**
+     * @return string|null
+     */
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string|null $firstname
+     * @return Contact
+     */
+    public function setFirstname(?string $firstname): Contact
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return Contact
+     */
+    public function setName(?string $name): Contact
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string|null $phone
+     * @return Contact
+     */
+    public function setPhone(?string $phone): Contact
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     * @return Contact
+     */
+    public function setEmail(?string $email): Contact
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string|null $message
+     * @return Contact
+     */
+    public function setMessage(?string $message): Contact
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+}
