007c8cc95752c396dbdd8e4c0ee38d80d746b7d0
Melissa Viltet <melissaviltet@gmail.com>
2018-11-29 13:09:28 +0100

    s02s20

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -38,7 +38,6 @@ class ContactController extends AbstractController
             return $this->redirectToRoute('contact');
         }
         return $this->render(
-            '/contact/index.html.twig', ['our_form' => $form->createView()]
-        );
+            '/contact/index.html.twig');
     }
 }
