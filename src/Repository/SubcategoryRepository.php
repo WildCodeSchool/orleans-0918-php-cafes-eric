d0d49d62d0d2f729ea572786c9a5f1d98c033b31
Candice Bourguignon <candice.bourguignon@gmail.com>
2018-12-04 14:30:35 +0100

    suppression du code commenté

--- src/Repository/SubcategoryRepository.php
+++ src/Repository/SubcategoryRepository.php
@@ -18,33 +18,4 @@ class SubcategoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Subcategory::class);
     }
-
-    // /**
-    //  * @return Subcategory[] Returns an array of Subcategory objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Subcategory
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
