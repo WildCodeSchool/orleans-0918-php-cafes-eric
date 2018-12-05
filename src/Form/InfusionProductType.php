283984f4c6107559a6f610e1dfa7bec99d933f1b
Amadou Kane <ak45amad@hotmail.fr>
2018-12-05 16:22:03 +0100

    Style de la page d'ajout

--- src/Form/InfusionProductType.php
+++ src/Form/InfusionProductType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\InfusionProduct;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +16,16 @@ class InfusionProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('tastingNote')
-            ->add('highlighted')
-            ->add('novelty')
-            ->add('category')
-        ;
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title',
+                'label' => 'Catégorie'
+            ])
+            ->add('name',TextType::class, ['label'=> 'Nom'])
+            ->add('description', TextType::class, ['label'=> 'Description'])
+            ->add('tastingNote', TextType::class, ['label'=> 'Note de dégustation', 'required' => false])
+            ->add('highlighted', CheckboxType::class, ['label'=> 'Produit du mois', 'required' => false])
+            ->add('novelty', CheckboxType::class, ['label'=> 'Nouveautée', 'required' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
