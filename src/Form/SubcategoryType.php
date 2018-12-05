c90d0314dbb57087a816609c462e7709351c9732
Candice Bourguignon <candice.bourguignon@gmail.com>
2018-12-05 08:27:00 +0100

    traduction des labels

--- src/Form/SubcategoryType.php
+++ src/Form/SubcategoryType.php
@@ -8,16 +8,17 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SubcategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, ['label'=> 'Nom'])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'title'
+                'choice_label' => 'title', 'label' =>'Catégorie',
             ])
         ;
     }
