ae1c966600b3bade89e3ba2ed4338df891d60b6f
Jessica Viel <jessica.mylady@gmail.com>
2018-12-05 16:42:01 +0100

    Suppression du code commenté

--- src/Repository/TeaProductRepository.php
+++ src/Repository/TeaProductRepository.php
@@ -18,33 +18,4 @@ class TeaProductRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, TeaProduct::class);
     }
-
-    // /**
-    //  * @return TeaProduct[] Returns an array of TeaProduct objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?TeaProduct
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
