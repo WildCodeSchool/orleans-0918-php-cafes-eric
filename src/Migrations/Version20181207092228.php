482eed5efb0fe4d07380836dfc02cca540a4ae11
Jessica Viel <jessica.mylady@gmail.com>
2018-12-07 13:30:42 +0100

    update

---
+++ src/Migrations/Version20181207092228.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181207092228 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tea DROP FOREIGN KEY FK_8E86D7B22750C5DD');
+        $this->addSql('DROP INDEX IDX_8E86D7B22750C5DD ON tea');
+        $this->addSql('ALTER TABLE tea DROP familly_tea_id');
+    }
+}
