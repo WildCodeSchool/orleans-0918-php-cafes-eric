49280b3fab89da0b58831f6dd89d8b5e0a263acb
Jessica Viel <jessica.mylady@gmail.com>
2018-12-08 10:44:44 +0100

    update

--- src/Form/TeaType.php
+++ src/Form/TeaType.php
@@ -3,11 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Category;
-use App\Entity\FamilyTea;
 use App\Entity\Tea;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,10 +19,6 @@ class TeaType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'title',
                 'label' => "Catégorie"])
-            ->add('family_tea', EntityType::class, [
-                'class' => FamilyTea::class,
-                'choice_label' => 'name',
-                'label' => "Famille"])
             ->add('name', TextType::class, [
                 'label' => 'Nom',
                 'help' => 'Ce nom doit être unique'])
