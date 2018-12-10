9d4e2d1ad2dbd21f5090076daec4685d75481b96
Candice Bourguignon <candice.bourguignon@gmail.com>
2018-12-10 09:57:47 +0100

    retrait du code commenté et ajout du lien dans la navbar

--- src/Repository/CoffeeRepository.php
+++ src/Repository/CoffeeRepository.php
@@ -18,33 +18,4 @@ class CoffeeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Coffee::class);
     }
-
-    // /**
-    //  * @return Coffee[] Returns an array of Coffee objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Coffee
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
