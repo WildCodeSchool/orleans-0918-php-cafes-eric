31d023dc3bec6bcaebfa1d198c709d7653ebb27b
Amadou Kane <ak45amad@hotmail.fr>
2018-12-12 10:53:26 +0100

    Style de la page d'ajout

--- src/Form/InfusionType.php
+++ src/Form/InfusionType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Entity\Infusion;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,10 @@ class InfusionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('ingredients')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'help' => 'Ce nom doit être unique'])
+            ->add('ingredients', TextType::class, ['label' => 'Ingrédients'])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'title',
