b489c741804fde84efaaae8bc68c414ce2935863
Candice Bourguignon <candice.bourguignon@gmail.com>
2018-12-18 10:13:14 +0100

    Affichage des catégories de café

--- src/Controller/CoffeeCategoryController.php
+++ src/Controller/CoffeeCategoryController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
 
 class CoffeeCategoryController extends AbstractController
 {
     /**
      * @Route("/coffee/category", name="coffee_category")
      */
-    public function index()
+    public function index(CategoryRepository $categoryRepository)
     {
         return $this->render('coffee_category/index.html.twig', [
             'controller_name' => 'CoffeeCategoryController',
+            'categories' => $categoryRepository->findByShelf(),
         ]);
     }
 }
