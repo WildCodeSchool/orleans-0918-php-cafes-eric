323d6c0f5b3165f42d03023316a86300ace6959f
Melissa Viltet <melissaviltet@gmail.com>
2018-12-18 15:17:06 +0100

    installation de la page grocery

---
+++ src/Controller/GroceryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GroceryController extends AbstractController
+{
+    /**
+     * @Route("/grocery", name="grocery")
+     */
+    public function index()
+    {
+        return $this->render('grocery/index.html.twig', [
+            'controller_name' => 'GroceryController',
+        ]);
+    }
+}
