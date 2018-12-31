de98ce9d90084329b7f95878651b7bf7f65c2be0
Julien D <julien.delbez@hotmail.fr>
2018-12-31 15:33:15 +0100

    ajout assert

--- src/Entity/Advertise.php
+++ src/Entity/Advertise.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdvertiseRepository")
@@ -18,6 +19,8 @@ class Advertise
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Assert\NotBlank
+     * @Assert\Length(max="250")
      */
     private $content;
 
