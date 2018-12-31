f7f547b181434ba83109b1c5af74809c6db45472
Jessica VIEL <jessica.mylady@gmail.com>
2018-12-31 19:31:02 +0100

    suppression d'une ligne commentée

--- src/Controller/WorkerController.php
+++ src/Controller/WorkerController.php
@@ -62,7 +62,6 @@ class WorkerController extends AbstractController
     public function edit(Request $request, Worker $worker): Response
     {
         $form = $this->createForm(WorkerType::class, $worker);
-//        dump($worker);die();
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
