e7ef8d3c3827c2356bc500fb078df4ab831f7d52
Jessica VIEL <jessica.mylady@gmail.com>
2019-01-01 16:00:00 +0100

    suppression d'un use

--- src/Form/WorkerType.php
+++ src/Form/WorkerType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Worker;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
