00ee220ce30da7ea07f3aea0a00496c026e50546
Candice Bourguignon <candice.bourguignon@gmail.com>
2019-01-03 16:30:40 +0100

    affichage des pays sur la page cafés

--- src/Controller/CoffeeCategoryController.php
+++ src/Controller/CoffeeCategoryController.php
@@ -2,31 +2,28 @@
 
 namespace App\Controller;
 
-use App\Repository\ShelfRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\CategoryRepository;
 use App\Repository\CoffeeRepository;
 
 class CoffeeCategoryController extends AbstractController
 {
     /**
      * @Route("/coffee/category", name="coffee_category")
-     * @param CategoryRepository $categoryRepository
-     * @param ShelfRepository $shelfRepository
+     * @param CoffeeRepository $coffeeRepository
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(
-        CategoryRepository $categoryRepository,
-        ShelfRepository $shelfRepository,
-        CoffeeRepository $coffeeRepository
-    ) {
-        $shelf = $shelfRepository->findOneBy(['shelfCode' => 'COFFEE']);
+    public function index(CoffeeRepository $coffeeRepository)
+    {
+        $coffees = $coffeeRepository->findBy([], ['country'=>'ASC']);
+        foreach ($coffees as $coffee) {
+            $country = $coffee->getCountry();
+            $category = $coffee->getCategory()->getTitle();
+            $coffeeByArea[$category][$country][] = $coffee;
+        }
 
         return $this->render('coffee_category/index.html.twig', [
-            'controller_name' => 'CoffeeCategoryController',
-            'categories' => $categoryRepository->findBy(['shelf' => $shelf->getId()]),
-            'coffees' => $coffeeRepository->listBy(),
+            'coffeeByArea'=>$coffeeByArea,
         ]);
     }
 }
