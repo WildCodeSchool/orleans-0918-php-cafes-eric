666e2804d5c0a05d221b6643240640cbd0615f90
Julien D <julien.delbez@hotmail.fr>
2019-01-03 15:04:13 +0100

    finalisation bandeau annonce

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Advertise;
 use App\Entity\Worker;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +17,13 @@ class HomeController extends AbstractController
         $workers = $this->getDoctrine()
             ->getRepository(Worker::class)
             ->findAll();
+        $advertises = $this->getDoctrine()
+            ->getRepository(Advertise::class)
+            ->findAll();
+
         return $this->render('home/index.html.twig', [
             'workers' => $workers,
+            'advertises' => $advertises,
         ]);
     }
 }
