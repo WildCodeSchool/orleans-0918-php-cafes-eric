c09ae390eceb9942e1ff6865da3ecc23e4b6f59b
Julien D <julien.delbez@hotmail.fr>
2019-01-08 19:42:36 +0100

    style de l'annonce fait

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Advertise;
 use App\Entity\Worker;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +17,12 @@ class HomeController extends AbstractController
         $workers = $this->getDoctrine()
             ->getRepository(Worker::class)
             ->findAll();
+        $advertise = $this->getDoctrine()
+            ->getRepository(Advertise::class)
+            ->findOneBy([]);
         return $this->render('home/index.html.twig', [
             'workers' => $workers,
+            'advertise' => $advertise,
         ]);
     }
 }
